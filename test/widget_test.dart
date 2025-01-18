8d045ebe06b1e33b8ac0e164f8cd28f96473c0aa
Wahyudi Setya Pradhana <210103172@mhs.udb.ac.id>
2025-01-18 23:45:17 +0700

    ubah

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kasirku/main.dart';
+import 'package:Kasirku/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
